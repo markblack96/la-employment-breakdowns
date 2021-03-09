3b488a9991cc8041bf9aa5f92702af8f665e6a0e
Mark Black <markblack96@gmail.com>
2021-03-09 15:39:16 -0600

    initial commit

---
+++ analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+all_states = pd.read_excel('data/state_M2019_dl.xlsx')
+louisiana = all_states[all_states['area_title'] == 'Louisiana']
+
+louisiana_employment_by_sector = louisiana[['occ_code', 'occ_title', 'tot_emp']][louisiana['occ_code'].str.contains('0000')]
+
+louisiana_employment_by_sector.sort_values(by='tot_emp', ascending=False).to_csv('la_emp_by_sec.csv', index=False)
+
+
